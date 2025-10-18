5ca758d9a08cbd5c657a3ecb34633d63aaddf2f2
Orges Isufi <orgesisufi1@hotmail.com>
2025-10-18 16:32:52 +0200

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/duobassrecords/' : '/',
+  base: '/duobassrecords/',
   plugins: [react()],
   server: {
     port: 3000,
